f3d4cb803ae62b9ea4e9fd1ab08b8e517c97f831
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-27 21:35:52 +0300

    update cars list

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,4 +1,10 @@
+import { useSelector } from "react-redux";
+
 const CarList = () => {
+  const { cars } = useSelector((state) => {
+    return { cars: state.cars.cars };
+  });
+  console.log(cars);
   return <div>CarList</div>;
 };
 
