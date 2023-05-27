bfedc0969022adb29d4f26a82c6556fa3070a86a
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-27 22:15:01 +0300

    add remove car function

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,11 +1,34 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { removeCar } from "../store";
 
 const CarList = () => {
   const cars = useSelector((state) => {
     return state.cars.cars;
   });
-  console.log(cars);
-  return <div>CarList</div>;
+
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      {cars.map((car) => {
+        return (
+          <div key={car.id} className="panel">
+            <p>
+              {car.name} - ${car.cost}
+            </p>
+            <button
+              className="button is-danger"
+              onClick={() => {
+                dispatch(removeCar(car.id));
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CarList;
