e5c204c885e007196cfaf0a6d2d19b5026b0cee0
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-28 18:03:00 +0300

    update code

--- src/components/CarSearch.jsx
+++ src/components/CarSearch.jsx
@@ -19,14 +19,12 @@ const CarSearch = () => {
     <div className="list-header">
       <h3 className="title is-3">My Cars</h3>
       <div className="search field is-horizontal">
-        <form>
-          <label>Search</label>
-          <input
-            className="input"
-            value={searchTerm}
-            onChange={handleSearchTermChange}
-          />
-        </form>
+        <label>Search</label>
+        <input
+          className="input"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        />
       </div>
     </div>
   );
