aaf28c63d956cdfe3f10eabf64463eb48e625257
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-28 19:31:15 +0300

    update code

--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -1,13 +1,13 @@
 import { useSelector } from "react-redux";
 
 const CarValue = () => {
-  const cost = useSelector(({ cars: { cars, searchTerm } }) => {
-    return cars
+  const cost = useSelector(({ cars: { cars, searchTerm } }) =>
+    cars
       .filter((car) =>
         car.name.toLowerCase().includes(searchTerm.toLowerCase())
       )
-      .reduce((acc, car) => acc + car.cost, 0);
-  });
+      .reduce((acc, car) => acc + car.cost, 0)
+  );
 
   return <div className="car-value">Total Cost : ${cost}</div>;
 };
