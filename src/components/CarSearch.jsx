51c7abf3e7aae2f8ecb556b74d571c46fbb4a00b
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-28 18:02:11 +0300

    activate car search bar

--- src/components/CarSearch.jsx
+++ src/components/CarSearch.jsx
@@ -1,11 +1,33 @@
+import { changeSearchTerm } from "../store/index";
+import { useDispatch, useSelector } from "react-redux";
+
 const CarSearch = () => {
+  const searchTerm = useSelector((state) => {
+    return state.cars.searchTerm;
+  });
+
+  const dispatch = useDispatch();
+
+  console.log("search term", searchTerm);
+
+  const handleSearchTermChange = (e) => {
+    e.preventDefault();
+    dispatch(changeSearchTerm(e.target.value));
+  };
+
   return (
-    <div className="">
-      <h1>My Cars</h1>
-      <form>
-        <label>Search</label>
-        <input />
-      </form>
+    <div className="list-header">
+      <h3 className="title is-3">My Cars</h3>
+      <div className="search field is-horizontal">
+        <form>
+          <label>Search</label>
+          <input
+            className="input"
+            value={searchTerm}
+            onChange={handleSearchTermChange}
+          />
+        </form>
+      </div>
     </div>
   );
 };
