9147f9357ee5b818f3c809c5c63628cee9ff1119
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-28 19:09:45 +0300

    add the total value

--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -1,5 +1,23 @@
+import { useSelector } from "react-redux";
+
 const CarValue = () => {
-  return <div>CarValue</div>;
+  const cost = useSelector((state) => {
+    return state.cars.cars.map((car) => {
+      return car.cost;
+    });
+  });
+  console.log(cost);
+  const sum = cost.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+
+  return (
+    <div>
+      {" "}
+      <h3>Total Value : ${sum}</h3>
+    </div>
+  );
 };
 
 export default CarValue;
